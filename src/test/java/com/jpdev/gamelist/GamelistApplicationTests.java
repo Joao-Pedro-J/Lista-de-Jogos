0258202713c7c81ea64169fe75adefdd9930de9e
João Pedro <matheusitalo125@gmail.com>
2025-01-25 15:35:49 -0300

    Projeto criado

---
+++ src/test/java/com/jpdev/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpdev.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
