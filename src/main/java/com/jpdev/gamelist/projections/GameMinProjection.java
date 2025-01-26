bd66f7e861240244f611b654843b334f4eef3814
João Pedro <matheusitalo125@gmail.com>
2025-01-26 14:24:48 -0300

    Homolaçção ok

--- src/main/java/com/jpdev/gamelist/projections/GameMinProjection.java
+++ src/main/java/com/jpdev/gamelist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.jpdev.gamelist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
